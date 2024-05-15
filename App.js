8cf4541b582e47e6c8bcadf4156c7a4982940706
DixitPansuriya <165665364+DixitPansuriya@users.noreply.github.com>
2024-05-15 15:17:19 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 // import Header from './Fake/Header';
-import Home from './Fake/Home';
-import Jewelry from './Fake/Jewelry';
-import Electronics from './Fake/Electronics';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Man from './Fake/Man';
-import Women from './Fake/Women';
-import Error from './Fake/Error';
+
 import Header from './Titan/Header'
 import Navbar from './Titan/Navbar'
 import Herosection from './Titan/Herosection'
 import Filter from './Titan/Filter'
 import Card from './Titan/Card'
+import Quality from './Titan/Quality';
+import Footer from './Titan/Footer';
+import LoginPage from './Titan/LoginPage';
+import Bestseller from './Titan/Bestseller';
+
 
 
 
@@ -40,6 +39,14 @@ export default function App() {
         <Herosection/>
         <Filter/>
         <Card/>
+        <Quality/>
+        <LoginPage/>
+<Bestseller/>
+<Footer/>
+
+
+       
+
     </div>
   );
 }
